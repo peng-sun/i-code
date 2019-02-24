a1a22c3264079a35c7da4c003f2023d2d7d31b04
peng-sun <psuncode@gmail.com>
2019-02-23 22:33:26 -0800

    Update examples.sh

--- bash/examples.sh
+++ bash/examples.sh
@@ -2,4 +2,4 @@
 if [ "$fname" = "a.txt" ] || [ "$fname" = "c.txt" ]; then echo yes; fi
 
 # single line while loop
-while true; do curl -X POST 'http://localhost:9031/tasks/bdbcache?action=getSequenceNumbers'; sleep 60; don
+while true; do curl -X POST 'http://localhost:9031/tasks/bdbcache?action=getSequenceNumbers'; sleep 60; done
