c65bcc76128936cbbd4a317a401280442f7a9f4c
peng-sun <psuncode@gmail.com>
2019-06-11 10:48:23 -0700

    Create use-http-status-codes-from-curl.sh

---
+++ bash/use-http-status-codes-from-curl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://coderwall.com/p/taqiyg/use-http-status-codes-from-curl
+
+while true
+do
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://example.com/poll-me)
+  if [ $STATUS -eq 200 ]; then
+    echo "Got 200! All done!"
+    break
+  else
+    echo "Got $STATUS :( Not done yet..."
+  fi
+  sleep 10
+done
